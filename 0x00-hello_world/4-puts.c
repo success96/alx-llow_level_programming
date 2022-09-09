dce645d57aaad6c3e22ac0837c2312761964a872
Success96 <lfawas6@gmail.com>
2022-09-08 19:35:05 -0700

    puts command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
- * /
-int main(void)
+ */
+int main (void)
 {
 
-	puts("\"Programming is loke building a multilingual puzzle");
-	retunr(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 
 }
