24a2bd480f7d12de421106576993c6895d5de7ea
Success96 <lfawas6@gmail.com>
2022-09-09 23:21:41 -0700

    alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,12 +11,14 @@
  */
 int main(void)
 {
-	char low;
+	char c = 'a';
 
-	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
-	for (low = 'A'; low <= 'Z'; low++)
-		putchar(low);
-	putchar('\n');
-	return (0);
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+		putchar('\n');
+		return (0);
+
 }
