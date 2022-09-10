ea4afd3b877e06c621a05f3c9b6b9a7da4256d38
Success96 <lfawas6@gmail.com>
2022-09-09 23:38:50 -0700

    jnd

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,9 +5,9 @@
 #include <time.h>
 
 /**
- *main – main block
- *Description: Get a random number and check its last digit, compare it with 5
- *Return: 0
+ * main – main block
+ * Description: Get a random number and check its last digit, compare it with 5
+ * Return: 0
  */
 int main(void)
 
