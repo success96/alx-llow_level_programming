6f849aa96c76af992af4888b642c0e203290f3ff
Olaosebikan Fawas <61049621+success96@users.noreply.github.com>
2022-09-11 18:54:01 +0100

    done

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+/**
+ *
+ * main – main block
+ * Description: Get a random number and check its last digit, compare it with 5 and verify its valuee
+ * Return: 0
+ */
+int main(void)
+
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+	for (low = 'A'; low <= 'Z'; low++)
+		putchar(low);
+	putchar('\n');
+	return (0);
+
+}
