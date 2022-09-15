ff05fcf2e1917a8924a344e6744ea5405f69fa84
Success96 <lfawas6@gmail.com>
2022-09-15 14:06:31 -0700

    Life is good

---
+++ 0x00-hello_world/hello_world.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ * main - Entry point for function main
+ *
+ * description - prints 'Helo world to the console'
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	printf("Hello, world\n");
+}
