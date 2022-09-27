fb3d02fe649057112439aea62353eca18ad81801
Success96 <lfawas6@gmail.com>
2022-09-26 21:05:41 -0700

    New commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,8 +1,10 @@
 #include "main.h"
 
 /**
+ *description - function is expected to print 10 times
  * print_diagonal - check for a digit
  *@n : number of \\ to be printed
+ *@c : number of \\ to be printed
  * Return:void
  */
 void more_numbers(void)
